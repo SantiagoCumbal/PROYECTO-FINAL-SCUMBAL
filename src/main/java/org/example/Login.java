78cf56e8ec8e60791c8831fe0398ec9653ce4c25
SantiagoCumbal <santiago.cumbal@epn.edu.ec>
2024-07-21 18:34:01 -0500

    Login1

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -4,4 +4,9 @@ import javax.swing.*;
 
 public class Login {
     public JPanel MainPanel;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton iniciarSesionButton;
+    private JButton registrarmeButton;
 }
